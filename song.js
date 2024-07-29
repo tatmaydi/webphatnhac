27fe98d943d246e077a761a46d31eb16740b4a3b
tatmaydi <tthdang2@gmail.com>
2024-07-30 01:26:40 +0700

    thoitiett

---
+++ song.js
@@ -0,0 +1,70 @@
+let songs = [
+    {
+        name:'Anh Thôi Nhân Nhựng',
+        path:'music/AnhThoiNhanNhuongCover.mp3',
+        artist:'Kiều Chi Cover',
+        imager:'img/AnhThoiNhanNhuongCover.jpg'
+    },
+    {
+        name:'Cẩm Tú Cầu',
+        path:'music/camtucau.mp3',
+        artist:'Cover Kiều Chi',
+        imager:'img/camtucau.jpg'
+    },
+    {
+        name:'Đơn Côi',
+        path:'music/DonCoiCover-KieuChi.mp3',
+        artist:'Kiều Chi Cover',
+        imager:'img/DonCoiCover-KieuChi.jpg'
+    },
+    
+    {
+        name:'Cô Phòng',
+        path:'music/CoPhong-HoQuangHieu.mp3',
+        artist:'Hồ Quang Hiếu',
+        imager:'img/tải xuống.jpg'
+    },
+    
+    {
+        name:'Níu Duyên',
+        path:'music/NiuDuyen.mp3',
+        artist:'Lê Bảo Bình',
+        imager:'img/NiuDuyen.jpg'
+    },
+    
+    {
+        name:'Quân Tử Ý',
+        path:'music/QuanTuY.mp3',
+        artist:'Lê Bảo Bình',
+        imager:'img/NiuDuyen.jpg'
+    },
+    
+    {
+        name:'Thích Thì Đến',
+        path:'music/ThichThiDenLofi.mp3',
+        artist:'Lê Bảo Bình',
+        imager:'img/ThichThiDenLofi.jpg'
+    },
+    
+    {
+        name:'Tìm Nhau Đau',
+        path:'music/TimNhauDau.mp3',
+        artist:'RayO',
+        imager:'img/TimNhauDau.jpg'
+    },
+    
+    {
+        name:'Tự Em Sai',
+        path:'music/TuEmSai.mp3',
+        artist:'Linh Hương Luz x IC MuSic',
+        imager:'img/TuEmSai.jpg'
+    },
+    
+    {
+        name:'Yêu Là Cưới',
+        path:'music/YeuLaCuoiDaiMeoRemixCover-KieuChi.mp3',
+        artist:'Đại Mèo Remix',
+        imager:'img/YeuLaCuoiDaiMeoRemixCover-KieuChi.jpg'
+    },
+    
+]
